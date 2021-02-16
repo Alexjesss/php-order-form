7080d4094d24dc687026407455360bfb7fe3e5ab
Alexandra Banica <banica9221@hotmail.be>
2021-02-16 18:02:37 +0100

    input validation succes step 1 and 2

--- index.php
+++ index.php
@@ -5,29 +5,81 @@ declare(strict_types=1);
 //we are going to use session variables so we need to enable sessions
 session_start();
 
+
+// Variables & constants
+
+$email_error = $street_error = $streetnumber_error = $city_error = $zipcode_error = "";
+$email = $street = $streetnumber = $city = $zipcode = "";
+const max_number = 4;
+const street_number = 5;
+
 // Required features
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-//,$_POST["street"],$_POST["streetnumber"], $_POST["city"], $_POST["zipcode"]
+    if (empty($_POST["street"])) {
+        $street_error = "street name is required";
+    }
+        elseif (!empty($_POST["street"]) && is_numeric($_POST["street"])) {
+                $street_error = "text characters only";
+        }
+    else {
+        $street = modified_input($_POST["street"]);
+        $_SESSION["street"] = $street;
+    }
 
-//Step 1
-
-
-
-if (isset($_POST["email"],$_POST["street"],$_POST["streetnumber"], $_POST["city"], $_POST["zipcode"]) && $_POST["email"] != "" && $_POST["street"] != "" && $_POST["streetnumber"] != "" && $_POST["city"]!= "" && $_POST["zipcode"] != "") {
-    if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
-        echo 'succes';
-        $Email = $_POST["email"];
+    if (!empty($_POST["streetnumber"]) && !is_numeric($_POST["streetnumber"])) {
+        $streetnumber_error = "Needs to only consist of numbers";
+    } elseif (empty($_POST["streetnumber"])) {
+        $streetnumber_error = "street number is required";
+    } elseif (mb_strlen($_POST["streetnumber"]) > street_number) {
+        $streetnumber_error = "Street number exceeds the max value";
     } else {
-        echo 'Your email address is not correct';
+        $streetnumber = modified_input($_POST["streetnumber"]);
+        $_SESSION["streetnumber"] = $streetnumber;
+    }
+
+    if (empty($_POST["city"])) {
+        $city_error = "city name is required";
+    }
+    elseif (!empty($_POST["street"]) && is_numeric($_POST["city"])) {
+        $city_error = "text characters only";
+    }
+    else {
+        $city = modified_input($_POST["city"]);
+        $_SESSION["city"] = $city;
+    }
+
+    if (!empty($_POST["zipcode"]) && !is_numeric($_POST["zipcode"])) {
+        $zipcode_error = "Needs to only consist of numbers";
+    } elseif (empty($_POST["zipcode"])) {
+        $zipcode_error = "zipcode is required";
+    } elseif (mb_strlen($_POST["zipcode"]) != max_number) {
+        $zipcode_error = "Needs to be 4 digits";
+    } else {
+        $zipcode = modified_input($_POST["zipcode"]);
+        $_SESSION["zipcode"] = $zipcode;
+    }
+
+    if (empty($_POST["email"])) {
+        if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+            $email_error = "required";
+        } else {
+            $email = modified_input($_POST["email"]);
+            $_SESSION["email"] = $email;
+        }
     }
 }
 
-function proc_input($input) {
+function modified_input($input)
+{
     $input = trim($input);
     $input = stripslashes($input);
     $input = htmlspecialchars($input);
     return $input;
 }
+
+
+
 
 
 function whatIsHappening()
@@ -61,3 +113,4 @@ $products = [
 $totalValue = 0;
 
 require 'form-view.php';
+
