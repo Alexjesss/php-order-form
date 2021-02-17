f001c6543d2d35e4aae5ff62a40adf4276bddc50
Alexandra Banica <banica9221@hotmail.be>
2021-02-17 12:15:15 +0100

    adjustments in input values

--- form-view.php
+++ form-view.php
@@ -15,10 +15,10 @@
     <nav>
         <ul class="nav">
             <li class="nav-item">
-                <a class="nav-link active" href="?food=1">Order food</a>
+                <a class="nav-link active" href="?food=0">Order food</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="?food=0">Order drinks</a>
+                <a class="nav-link" href="?food=1">Order drinks</a>
             </li>
         </ul>
     </nav>
@@ -26,8 +26,9 @@
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="email">E-mail:</label>
-                <input type="text" id="email" name="email" class="form-control"/>
-                <span>*<?php echo $email_error;?></span>
+                <input type="text" id="email" name="email" class="form-control" value="<?php echo $_POST["email"] ?? $_SESSION["email"] ?? '';?>"/>
+                <!-- above code is shortcut for < ? php echo isset($_POST['name']) ? $_POST['name'] : ''; ? > , ?? = if isset? do this, code after ?? = else, '' = empty string  -->
+                <span><?php echo $email_error;?></span>
             </div>
             <div></div>
         </div>
@@ -38,25 +39,25 @@
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="street">Street:</label>
-                    <input type="text" name="street" id="street" class="form-control" value="<?php echo $street;?>">
-                    <span>*<?php echo $street_error;?></span>
+                    <input type="text" name="street" id="street" class="form-control" value="<?php echo $_POST["street"] ?? $_SESSION["street"] ?? '';?>">
+                    <span><?php echo $street_error;?></span>
                 </div>
                 <div class="form-group col-md-6">
                     <label for="streetnumber">Street number:</label>
-                    <input type="text" id="streetnumber" name="streetnumber" class="form-control" value="<?php echo $streetnumber;?>">
-                    <span>*<?php echo $streetnumber_error;?></span>
+                    <input type="text" id="streetnumber" name="streetnumber" class="form-control" value="<?php echo $_POST["streetnumber"] ?? $_SESSION["streetnumber"] ?? '';?>">
+                    <span><?php echo $streetnumber_error;?></span>
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="city">City:</label>
-                    <input type="text" id="city" name="city" class="form-control" value="<?php echo $city;?>">
-                    <span>*<?php echo $city_error;?></span>
+                    <input type="text" id="city" name="city" class="form-control" value="<?php echo $_POST["city"] ?? $_SESSION["city"] ?? '';?>">
+                    <span><?php echo $city_error;?></span>
                 </div>
                 <div class="form-group col-md-6">
                     <label for="zipcode">Zipcode</label>
-                    <input type="text" id="zipcode" name="zipcode" class="form-control" value="<?php echo $zipcode;?>">
-                    <span>*<?php echo $zipcode_error;?></span>
+                    <input type="text" id="zipcode" name="zipcode" class="form-control" value="<?php echo $_POST["zipcode"] ?? $_SESSION["zipcode"] ?? '';?>">
+                    <span><?php echo $zipcode_error;?></span>
                 </div>
             </div>
         </fieldset>
@@ -75,7 +76,7 @@
             Express delivery (+ 5 EUR) 
         </label>
             
-        <button type="submit" class="btn btn-primary">Order!</button>
+        <button type="submit" class="btn btn-primary" name="orderButton">Order!</button>
     </form>
 
     <footer>You already ordered <strong>&euro; <?php echo $totalValue ?></strong> in food and drinks.</footer>
